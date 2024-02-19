38cc3221afca78239e0b6b23f1ec5f27491a9ed9
Keshav Sharma <kshar26@uwo.ca>
2024-02-18 23:32:23 -0500

    Updates

--- assets/LandingPage-BOcWj0PG.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CcN3QIbW.js";const n=()=>t.jsx("div",{style:{margin:"auto",width:"500px"},children:t.jsx("h1",{children:"Commimg Soon"})});export{n as default};
--- src/components/LandingPage/LandingPage.jsx
+++
@@ -1,8 +0,0 @@
-const LandingPage = () => {
-  return (
-    <div style={{ margin: "auto", width: "500px" }}>
-      <h1>Commimg Soon</h1>
-    </div>
-  );
-};
-export default LandingPage;
